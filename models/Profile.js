cfaeff65325de8d35adb6f443017bdcbaaa1d208
Kelum Senanayake <kelumkps@yahoo.com>
2015-08-16 08:12:21 +0800

    Configure Mandril email

--- models/Profile.js
+++ models/Profile.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 var ProfileSchema = new mongoose.Schema({
-  emailaddress: String,
+  email: String,
   updated_at: { type: Date, default: Date.now },
 });
 
--- routes/images.js
+++ routes/images.js
@@ -4,18 +4,46 @@ var Profile = require('../models/Profile.js');
 var mongoose = require('mongoose');
 var Image = require('../models/Image.js');
 
+var mandrill = require('mandrill-api/mandrill');
+var mandrill_client = new mandrill.Mandrill('qCvRgqX5FE4Kk0L3M-3wrQ');
+
 /* GET users listing. */
 //router.get('/', function(req, res) {
 //  res.send('respond with a resource');
 //});
 
 router.get('/', function(req, res, next) {
+    var message1 = {
+        "html": "<h1></h1><br/><p> </p><p>Applied Date: "+new Date()+"</p>",
+        "text": " - : "+"   Applied Date: "+new Date(),
+        "subject": "Urgent Help Required!",
+        "from_email": "crimepush@dev.com",
+        "from_name": "Crime Push Team",
+        "headers": {
+            "Reply-To": "crimepush@dev.com"
+        },
+        "important": false,
+        "track_opens": true,
+        "track_clicks": true,
+        "auto_text": true
+    };
+
+
   Image.find(function (err, images) {
     if (err) return next(err);
-    //res.json(images);
-    	Profile.find({}, {'emailaddress': 1, '_id': 0}, function (err, emails){
+    	Profile.find({}, {'email': 1, '_id': 0}, function (err, emails){
 		    if (err) return next(err);
+          message1['to'] = emails;
+          mandrill_client.messages.send({"message": message1}, function(result) {
+              console.log(result);
+
+          }, function(e) {
+              // Mandrill returns the error as an object with name and message keys
+              console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
+              // A mandrill error occurred: Unknown_Subaccount - No subaccount exists with the id 'customer-123'
+          });
 		    res.json(emails);	
+
 	   });
   });
 });
